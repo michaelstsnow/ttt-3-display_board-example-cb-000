613603d15ba733e4ba3da4bbbdbda987ba52ed15
michaelstsnow <michael.st.snow@gmail.com>
2018-05-17 23:53:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   #This is a comment 
+  print "   |   |   "
+  print "-----------"
+  print "   |   |   "
+  print "-----------"
+  print "   |   |   "
 end
